dd835907bdc3b2c6c41495e1949552e4324e836b
this-is-spear <rjsckdd12@gmail.com>
2023-02-11 18:37:35 +0900

    Test : 프로젝트 환경 테스트

---
+++ src/main/java/numble/bankingapi/security/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package numble.bankingapi.security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((authorize) -> authorize
+            .requestMatchers("/hello").permitAll()
+            .anyRequest().denyAll()
+        )
+        .httpBasic(withDefaults())
+        .formLogin(withDefaults());
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService users() {
+    UserBuilder users = User.withDefaultPasswordEncoder();
+    UserDetails admin = users
+        .username("admin")
+        .password("password")
+        .roles("USER", "ADMIN")
+        .build();
+    return new InMemoryUserDetailsManager(admin);
+  }
+}
