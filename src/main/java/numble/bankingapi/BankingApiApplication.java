f6f7c6c5821fb9800920ceb2eb6a5327300b5b39
this-is-spear <rjsckdd12@gmail.com>
2023-02-11 11:59:03 +0900

    Refact : 프로젝트 린트 체크

--- src/main/java/numble/bankingapi/BankingApiApplication.java
+++ src/main/java/numble/bankingapi/BankingApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankingApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BankingApiApplication.class, args);
+  }
 
 }
