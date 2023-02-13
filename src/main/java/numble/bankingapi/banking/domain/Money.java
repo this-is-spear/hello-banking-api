696c3666b6a41c9849b8692c7324eb83b5456519
this-is-spear <rjsckdd12@gmail.com>
2023-02-14 02:26:17 +0900

    Refact : Money 리팩토링

--- src/main/java/numble/bankingapi/banking/domain/Money.java
+++ src/main/java/numble/bankingapi/banking/domain/Money.java
@@ -3,17 +3,20 @@ package numble.bankingapi.banking.domain;
 public class Money {
 	private long amount;
 
-	private Money() {
-	}
+	private Money() {/*no-op*/}
 
 	public Money(long amount) {
-		if (amount < 0) {
-			throw new NotNegativeException("금액은 0원 이상이어야 합니다.");
-		}
+		validateAmount(amount);
 		this.amount = amount;
 	}
 
 	public long getAmount() {
 		return amount;
+	}
+
+	private void validateAmount(long amount) {
+		if (amount < 0) {
+			throw new NotNegativeException("금액은 0원 이상이어야 합니다.");
+		}
 	}
 }
