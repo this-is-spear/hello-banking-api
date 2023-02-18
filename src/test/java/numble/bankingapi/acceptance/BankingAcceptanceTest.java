c25c867a20418677168d5ac3d4e02fa892b86920
this-is-spear <rjsckdd12@gmail.com>
2023-02-19 00:13:26 +0900

    Test : BankingAcceptanceTest 진행
    ####작업 내용###
    - 성공되도록 수정
    - 시나리오 테스트 추가
    - 가독성을 위해 한글화 진행

--- src/test/java/numble/bankingapi/acceptance/BankingAcceptanceTest.java
+++ src/test/java/numble/bankingapi/acceptance/BankingAcceptanceTest.java
@@ -1,15 +1,16 @@
 package numble.bankingapi.acceptance;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,24 +22,60 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import numble.bankingapi.DataLoader;
 import numble.bankingapi.util.DatabaseCleanup;
 
-@Disabled
-@AutoConfigureMockMvc
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BankingAcceptanceTest {
 
+	private static final String 이메일 = "member@email.com";
+	private static final String 비밀번호 = "password";
+	private static final String AMOUNT = "$.balance.amount";
 	@Autowired
 	MockMvc mockMvc;
 	@Autowired
 	ObjectMapper objectMapper;
 	@Autowired
 	private DatabaseCleanup databaseCleanup;
+	@Autowired
+	private DataLoader dataLoader;
+	private Map<String, String> accountNumber;
 
 	@BeforeEach
 	public void setUp() {
 		databaseCleanup.execute();
+		accountNumber = dataLoader.loadData();
+	}
+
+	/**
+	 * 사용자는 5천원을 입금하고 5천원을 출금하면 잔액이 0원 남는다.
+	 *
+	 * @Given : 사용자는 5천원을 입금하고
+	 * @When : 5천원을 출금하면
+	 * @Then : 잔액이 0원 남는다.
+	 */
+	@Test
+	void deposit_and_withdraw_5000() throws Exception {
+		// given
+		String 나의계좌 = accountNumber.get("member");
+
+		long 입금할_돈 = 5_000L;
+		ResultActions 계좌_입금 = 계좌_입금_요청(나의계좌, 입금할_돈, 이메일, 비밀번호);
+		계좌_입금.andExpect(status().isOk());
+
+		계좌_조회_요청(나의계좌, 이메일, 비밀번호).andExpect(
+			jsonPath(AMOUNT).value(입금할_돈));
+
+		// when
+		long 출금할_돈 = 5_000L;
+		ResultActions 계좌_출금 = 계좌_출금_요청(나의계좌, 출금할_돈, 이메일, 비밀번호);
+		계좌_출금.andExpect(status().isOk());
+
+		// then
+		계좌_조회_요청(나의계좌, 이메일, 비밀번호).andExpect(
+			jsonPath(AMOUNT).value(입금할_돈 - 출금할_돈));
 	}
 
 	/**
@@ -50,21 +87,28 @@ public class BankingAcceptanceTest {
 	 */
 	@Test
 	void transfer_5000() throws Exception {
-		String fromAccountNumber = "123-234-543-12";
-		String toAccountNumber = "123-234-543-12";
-
 		// given
+		String 나의계좌 = accountNumber.get("member");
+		String 상대방계좌 = accountNumber.get("admin");
+
 		long depositMoney = 10_000L;
-		ResultActions deposit = deposit(fromAccountNumber, depositMoney);
-		deposit.andExpect(status().isOk());
+		ResultActions 계좌_입금 = 계좌_입금_요청(나의계좌, depositMoney, 이메일, 비밀번호);
+		계좌_입금.andExpect(status().isOk());
+
+		계좌_조회_요청(나의계좌, 이메일, 비밀번호).andExpect(
+			jsonPath(AMOUNT).value(depositMoney));
 
 		// when
 		long transferMoney = 5_000L;
-		ResultActions transfer = transfer(fromAccountNumber, toAccountNumber, transferMoney);
-		transfer.andExpect(status().isOk());
+		ResultActions 계좌_이체 = 계좌_이체_요청(나의계좌, 상대방계좌, transferMoney, 이메일, 비밀번호);
+		계좌_이체.andExpect(status().isOk());
 
 		// then
-		getAccountHistory(fromAccountNumber).andExpect(jsonPath("$.balance").value(depositMoney - transferMoney));
+		계좌_조회_요청(나의계좌, 이메일, 비밀번호).andExpect(
+			jsonPath(AMOUNT).value(depositMoney - transferMoney));
+
+		계좌_조회_요청(상대방계좌, "admin@gmail.com", "password").andExpect(
+			jsonPath(AMOUNT).value(transferMoney));
 	}
 
 	/**
@@ -76,20 +120,23 @@ public class BankingAcceptanceTest {
 	 */
 	@Test
 	void transfer_failed() throws Exception {
-		String fromAccountNumber = "123-234-543-12";
-		String toAccountNumber = "123-234-543-12";
-
 		// given
+		String 나의계좌 = accountNumber.get("member");
+		String 상대방계좌 = accountNumber.get("admin");
+
 		long depositMoney = 3_000L;
-		ResultActions deposit = deposit(fromAccountNumber, depositMoney);
-		deposit.andExpect(status().isOk());
+		ResultActions 계좌_입금 = 계좌_입금_요청(나의계좌, depositMoney, 이메일, 비밀번호);
+		계좌_입금.andExpect(status().isOk());
+
+		계좌_조회_요청(나의계좌, 이메일, 비밀번호).andExpect(
+			jsonPath(AMOUNT).value(depositMoney));
 
 		// when
 		long transferMoney = 5_000L;
-		ResultActions transfer = transfer(fromAccountNumber, toAccountNumber, transferMoney);
+		ResultActions 계좌_이체 = 계좌_이체_요청(나의계좌, 상대방계좌, transferMoney, 이메일, 비밀번호);
 
 		// then
-		transfer.andExpect(status().isBadRequest());
+		계좌_이체.andExpect(status().isBadRequest());
 	}
 
 	/**
@@ -101,65 +148,93 @@ public class BankingAcceptanceTest {
 	 */
 	@Test
 	void transfer_concurrency_200_times() throws Exception {
-		String fromAccountNumber = "123-234-543-12";
-		String toAccountNumber = "123-234-543-12";
-
 		// given
-		long depositMoney = 3_000L;
-		ResultActions deposit = deposit(fromAccountNumber, depositMoney);
-		deposit.andExpect(status().isOk());
+		String 나의계좌 = accountNumber.get("member");
+		String 상대방계좌 = accountNumber.get("admin");
+
+		long depositMoney = 1_000_000L;
+		ResultActions 계좌_입금 = 계좌_입금_요청(나의계좌, depositMoney, 이메일, 비밀번호);
+		계좌_입금.andExpect(status().isOk());
+
+		계좌_조회_요청(나의계좌, 이메일, 비밀번호).andExpect(
+			jsonPath(AMOUNT).value(depositMoney));
 
 		// when
-		long transferMoney = 5_000L;
-		transfer200Times(fromAccountNumber, toAccountNumber, transferMoney);
+		long transferMoney = 1_000L;
+		int times = 200;
+		transferManyTimes(나의계좌, 상대방계좌, transferMoney, times, 이메일, 비밀번호);
 
 		// then
-		getAccountHistory(fromAccountNumber).andExpect(jsonPath("$.balance").value(depositMoney - transferMoney));
+		계좌_조회_요청(나의계좌, 이메일, 비밀번호).andExpect(
+			jsonPath(AMOUNT).value(depositMoney - transferMoney * times));
 	}
 
-	private void transfer200Times(String fromAccountNumber, String toAccountNumber, long transferMoney) {
-		int times = 200;
-		int threads = 20;
+	private void transferManyTimes(String fromAccountNumber, String toAccountNumber, long transferMoney, int times,
+		String username, String password) throws InterruptedException {
+		int threads = 2;
+		CountDownLatch latch = new CountDownLatch(times);
+
 		ExecutorService executorService = Executors.newFixedThreadPool(threads);
 
 		for (int i = 0; i < times; i++) {
 			executorService.execute(() -> {
 				try {
-					transfer(fromAccountNumber, toAccountNumber, transferMoney);
+					계좌_이체_요청(fromAccountNumber, toAccountNumber, transferMoney, username, password);
+					latch.countDown();
 				} catch (Exception e) {
 					throw new RuntimeException(e);
 				}
 			});
 		}
+		latch.await();
 	}
 
-	private ResultActions getAccountHistory(String fromAccountNumber) throws Exception {
-		return mockMvc.perform(
-			get("/account/{accountNumber}/history", fromAccountNumber)
-		);
-	}
-
-	private ResultActions transfer(String fromAccountNumber, String toAccountNumber, long transferMoney) throws
-		Exception {
+	private ResultActions 계좌_이체_요청(String fromAccountNumber, String toAccountNumber, long transferMoney,
+		String username, String password) throws Exception {
 		Map<String, Object> transferParams = new HashMap<>();
 		transferParams.put("toAccountNumber", toAccountNumber);
-		transferParams.put("money", transferMoney);
+		transferParams.put("amount", transferMoney);
 
 		return mockMvc.perform(
 			post("/account/{accountNumber}/transfer", fromAccountNumber)
+				.with(user(username).password(password).roles("MEMBER"))
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(transferParams))
 		);
 	}
 
-	private ResultActions deposit(String accountNumber, long depositMoney) throws Exception {
+	private ResultActions 계좌_입금_요청(String accountNumber, long depositMoney, String username, String password)
+		throws Exception {
 		Map<String, Object> depositParams = new HashMap<>();
-		depositParams.put("money", depositMoney);
+		depositParams.put("amount", depositMoney);
 
 		return mockMvc.perform(
 			post("/account/{accountNumber}/deposit", accountNumber)
+				.with(user(username).password(password).roles("MEMBER"))
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsString(depositParams))
 		);
 	}
+
+	private ResultActions 계좌_출금_요청(String accountNumber, long depositMoney, String username, String password)
+		throws Exception {
+		Map<String, Object> depositParams = new HashMap<>();
+		depositParams.put("amount", depositMoney);
+
+		return mockMvc.perform(
+			post("/account/{accountNumber}/withdraw", accountNumber)
+				.with(user(username).password(password).roles("MEMBER"))
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(depositParams))
+		);
+	}
+
+	private ResultActions 계좌_조회_요청(String accountNumber, String username, String password) throws Exception {
+		return mockMvc.perform(
+			get("/account/{accountNumber}/history", accountNumber)
+				.with(user(username).password(password).roles("MEMBER"))
+				.accept(MediaType.APPLICATION_JSON)
+		);
+	}
+
 }
