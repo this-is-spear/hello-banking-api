f913ce60c454f294cc4c5143027ba4b33b17a86f
this-is-spear <rjsckdd12@gmail.com>
2023-02-19 21:30:29 +0900

    Refact : NumbleAlarmService 수정
    ### 작업 내용###
    - 비동기 요청으로 수정

--- src/main/java/numble/bankingapi/alarm/infra/NumbleAlarmService.java
+++ src/main/java/numble/bankingapi/alarm/infra/NumbleAlarmService.java
@@ -1,11 +1,13 @@
 package numble.bankingapi.alarm.infra;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import numble.bankingapi.alarm.domain.AlarmService;
 
 @Service
 public class NumbleAlarmService implements AlarmService {
+	@Async
 	public void notify(Long userId, String message) {
 		try {
 			Thread.sleep(500);
