cae1f7a7a7f4285e3f76efd8f8b716609e8bce75
this-is-spear <rjsckdd12@gmail.com>
2023-02-19 11:18:22 +0900

    Refact : ConcurrencyManagerWithSynchronized 수정
    ###작업 내용###
    - 로그 추가

--- src/main/java/numble/bankingapi/concurrency/ConcurrencyManagerWithSynchronized.java
+++ src/main/java/numble/bankingapi/concurrency/ConcurrencyManagerWithSynchronized.java
@@ -2,12 +2,17 @@ package numble.bankingapi.concurrency;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ConcurrencyManagerWithSynchronized implements ConcurrencyManager {
 	@Override
 	public void executeWithLock(String lockName, Runnable runnable) {
+		log.debug("Start Concurrency Control : {}", lockName);
 		synchronized (this) {
 			runnable.run();
 		}
+		log.debug("End Concurrency Control : {}", lockName);
 	}
 }
