40a7a5a2e4099f7fb6f381ec3862c77f7744a9be
this-is-spear <rjsckdd12@gmail.com>
2023-06-02 01:12:44 +0900

    Refact : 동시성 제어 코드 수정

--- src/main/java/numble/bankingapi/concurrency/ConcurrencyManager.java
+++ src/main/java/numble/bankingapi/concurrency/ConcurrencyManager.java
@@ -1,5 +1,7 @@
 package numble.bankingapi.concurrency;
 
 public interface ConcurrencyManager {
-	void executeWithLock(String lockName, String number, Runnable runnable);
+	void executeWithLock(String lockName1, String lockName2, Runnable runnable);
+
+	void executeWithLock(String lockName, Runnable runnable);
 }
